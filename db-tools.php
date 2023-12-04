4bfc47333010d21d37569a62281f35ad7f488b3a
3041TAN-16 <3041TAN-16@boronkay.local>
2023-12-04 09:19:14 +0100

    initial commit

---
+++ db-tools.php
@@ -0,0 +1,72 @@
+<?php
+
+
+
+function createMaker($mysqli, $maker)
+{
+    $result = $mysqli->query("INSERT INTO makers (name) VALUES ('$maker')");
+    if (!$result)
+    {
+        echo "Hiba történt a $maker beszúrása közben";
+    }
+    
+    return $result;
+}
+
+function updateMaker($mysqli, $data)
+{
+    $makerName = $data['name'];
+    $result = $mysqli->query("UPDATE makers SET name={$data['name']}");
+    
+    if (!$result)
+    {
+        echo "Hiba történt a $makerName beszúrása közben";
+        return $result;
+    }
+    $maker = getMakerByName($mysqli, $makerName);
+    return $result;
+}
+
+function getMaker($mysqli, $id)
+{
+    $result = $mysqli->query("SELECT * FROM makers WHERE id=$id");
+    $maker = $result->fetch_assoc();
+
+    return $maker;
+}
+
+function getMakerByName($mysqli, $name)
+{
+    $result = $mysqli->query("SELECT * FROM makers WHERE name=$name");
+    $maker = $result->fetch_assoc();
+
+    return $maker;
+
+
+}
+
+function delMaker($mysqli, $id)
+{
+    $result = $mysqli->query("DELETE makers WHERE id=$id");
+    
+
+    return $result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
